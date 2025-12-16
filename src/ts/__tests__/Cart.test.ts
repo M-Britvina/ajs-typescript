d5c075839561970d4434be9964d8312968732bb3
Mariya Britvina <m.sea7@mail.ru>
2025-12-17 00:54:24 +0300

    Домашнее задание к лекции "TypeScript". Новые типы

---
+++ src/ts/__tests__/Cart.test.ts
@@ -0,0 +1,28 @@
+import Cart from '../service/Cart';
+import Book from '../domain/Book';
+import Movie from '../domain/Movie';
+import MusicAlbum from '../domain/MusicAlbum';
+
+test('new card should be empty', () => {
+  const cart = new Cart();
+
+  expect(cart.items.length).toBe(0);
+});
+
+test('add to cart', () => {
+  const cart = new Cart();
+
+  const book = new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225);
+  const musicAlbum = new MusicAlbum(1008, 'Meteora', 'Linkin Park', 900);
+  const movie = new Movie(3, "Мстители", 300, "The avengers", "США", "Avengers Assemble!", ["фантастика", "боевик", "фэнтези", "приключения"], 137);
+  
+  cart.add(book);
+  cart.add(musicAlbum);
+  cart.add(movie);
+  
+  expect(cart.items.length).toBe(3);
+  expect(cart.items[0]).toEqual(book);
+  expect(cart.items[1]).toEqual(musicAlbum);
+  expect(cart.items[2]).toEqual(movie);
+
+});
